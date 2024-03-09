730d6eef214d6772c448de3fb9bf42fed8f93b9d
SINITAMBIRIVOUTIN Bastien <bastien.sini@gmail.com>
2024-03-08 23:15:24 -0400

    NETTOYAGE DU JS
    factorisation + fix des msg de fin de partie

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ async function Inscription() {
         const apicall = await fetch(urlAPI + "inscription&pseudo=" + pseudoInput.value);
         const reponse = await apicall.json();
 
-        console.log(reponse);
+        console.log("Inscription : ", reponse);
 
         if (reponse.etat == etat.KO) {
             message.textContent = "Pseudo déjà pris";
@@ -54,9 +54,10 @@ async function Inscription() {
     window.location.replace("menu.html");
 }
 
+// Reconnexion avec un pseudo précédemment enregistré
 function Reconnexion() {
     window.location.replace("menu.html");
-    
+    console.log("Reconnexion");
     console.log("Pseudo : " + localStorage.getItem("pseudo"));
     console.log("Identifiant : " + localStorage.getItem("id"));
 }
@@ -72,74 +73,63 @@ function showWhoIsConnected() {
     document.getElementById("connected").textContent = phraseConnecte + pseudo;
 }
 
+// Retour à l'inscription
 function Quitter() {
-    // Retour à l'inscription
-    Giveup();
+    const etatSearch = document.getElementById("etat");
+
+    if (etatSearch.textContent != "") {
+        // Demande un abandon de la recherche uniquement si elle à été lancée
+        Giveup();
+    }
+    
     console.log("Quitter");
     window.location.replace("index.html");
 }
 
 async function Play() {
     // Lancer la partie
-    const Id = localStorage.getItem("id");
-    const apicall = await fetch(urlAPI + "participer&identifiant=" + Id);
+    const id = localStorage.getItem("id");
+    const apicall = await fetch(urlAPI + "participer&identifiant=" + id);
     const reponse = await apicall.json();
-    console.log(reponse);
 
-    if (reponse.etat == "En attente") {
-        console.log("en attente ?");
-        let etatSearch = document.getElementById("etat");
+    console.log("Participer : ", reponse);
+
+    const etatSearch = document.getElementById("etat");
+
+    if (reponse.etat == etat.ENATTENTE) {
         etatSearch.textContent = "Recherche d'aversaire...";
         etatSearch.style.animation = "rotate-hor-center 1.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) 0.5s infinite both";
         WaitingGame();
     }
-    else if (reponse.etat == "En cours") {
+    else if (reponse.etat == etat.ENCOURS) {
         localStorage.setItem("joueur", parseInt(reponse.joueur));
         window.location.replace("jeu.html");
     }
-
-
 }
 
+// Rafraîchit les informations de la partie
 async function refresh() {
     const Id = localStorage.getItem("id");
     const pseudo = localStorage.getItem("pseudo");
-
-    if (gameEnd) {
-        return;
-    }
+    const joueurIndex = localStorage.getItem("joueur");
 
     const apicall = await fetch(urlAPI + "statut&identifiant=" + Id);
     const reponse = await apicall.json();
-    console.log("refresh");
-    console.log(apicall);
-    console.log(reponse);
+    console.log("Statut (Playing) : ", reponse);
 
     const h2J1 = document.getElementById('j1');
     const h2J2 = document.getElementById('j2');
 
-    h2J1.style.color = colorJ1;
-    h2J2.style.color = colorJ2;
-
-    if (reponse.etat == "En cours")  {
-
+    if (reponse.etat == etat.ENCOURS) {
         if (reponse.adversaire == null) {
             console.log("partie crash");
-            Giveup();
+            Quitter();
         }
+
+        monTour = (joueurIndex == reponse.tour);
+        h2J1.textContent = joueurIndex == 1 ? pseudo : reponse.adversaire;
+        h2J2.textContent = joueurIndex == 2 ? pseudo : reponse.adversaire;
         
-        if (reponse.joueur == "1") {
-            h2J1.textContent = reponse.pseudo;
-            h2J2.textContent = reponse.adversaire;
-        }
-        else {
-            h2J1.textContent = reponse.adversaire;
-            h2J2.textContent = reponse.pseudo;
-        }
-
-        const pseudoJ1 = h2J1.textContent;
-        const pseudoJ2 = h2J2.textContent;
-
         if (reponse.tour == "1") {
             h2J1.style.animation = "text-pop-up-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
             h2J2.style.animation = "";
@@ -149,49 +139,56 @@ async function refresh() {
             h2J2.style.animation = "text-pop-up-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
         }
 
-        monTour = (document.getElementById('j' + reponse.tour).textContent == pseudo);
-
-        updatePlateau(reponse.carte);
-        
-    }
+    } 
     else if (reponse.etat.includes("gagne")) {
-        if (reponse.carte != undefined) {
-            updatePlateau(reponse.carte);
-            GameEnd(false);
-        } else {
+        if (reponse.etat.includes(joueurIndex)) {
             GameEnd(null);
         }
-    }
-    else if (reponse.etat == "OK") {
+        else {
+            GameEnd(false);
+        }
+    } 
+    else if (reponse.etat == etat.OK) {
         window.location.replace("menu.html");
     }
+    else {
+        alert('crash server');
+    }
 
+    if (reponse.carte != undefined) {
+        updatePlateau(reponse.carte);
+    } 
 }
 
 function updatePlateau(carte) {
-    for (let row = 0; row < 6; row++) {
+    // met à jour le plateau avec la nouvelle carte
+    for (let ligne = 0; ligne < 6; ligne++) {
         for (let col = 0; col < 7; col++) {
-            const cellIndex = row * 7 + col + 1;
-            const cellValue = carte[row][col];
+            const cellIndex = ligne * 7 + col + 1;
+            const caseValue = carte[ligne][col];
 
-            const cell = document.getElementById(cellIndex);
-            // const imgPion = document.createElement("img");
+            const cellule = document.getElementById(cellIndex);
 
-            const portepion = document.createElement("div");
-            const pion = document.createElement("div");
+            // AJOUT DE NOUVEAU PION
+            if (cellule.innerHTML == "" && caseValue > 0) {
+                const portepion = document.createElement("div");
+                const pion = document.createElement("div"); 
 
-            if (cell.innerHTML == "") {
-                if (cellValue > 0) {
-                    portepion.classList.add("portePion"); portepion.classList.add("J"+cellValue);
-                    pion.classList.add("game-pion"); pion.classList.add("J"+cellValue);
-                    
-                    const distance = "-" + row + 1 * cell.offsetHeight + "px";
-                    console.log(distance);
-                    document.documentElement.style.setProperty("--distance", distance);
-                    portepion.style.animation = "pionFall 0.5s linear both";
-                    portepion.appendChild(pion);
-                    cell.appendChild(portepion);
-                }
+                // Ajout CSS pour le fond du pion
+                portepion.classList.add("portePion"); 
+                portepion.classList.add("J" + caseValue);
+                // Ajout CSS pour le coeur du pion
+                pion.classList.add("game-pion"); 
+                pion.classList.add("J" + caseValue);
+                
+                // Calcul de la distance entre le haut du plateau et la position finale du NOUVEAU pion
+                const distance = "-" + ligne + 1 * cellule.offsetHeight + "px";
+                document.documentElement.style.setProperty("--distance", distance);
+                // Ajout de l'animation
+                portepion.style.animation = "pionFall 0.5s linear both";
+                // Ajout du pion à la case correspondante
+                portepion.appendChild(pion);
+                cellule.appendChild(portepion);
             }
         }
     }
@@ -203,7 +200,6 @@ function sleep(ms) {
 
 function addInteractions() {
     const animation = "cell-pulse-J" + localStorage.getItem("joueur") + " 1s ease-in-out 0s infinite";
-    console.log(animation);
 
     for (var i=1; i<=42; i++) {
         let colonne = (i % 7==0)? 7 : i % 7;
@@ -213,22 +209,25 @@ function addInteractions() {
         });
 
         td.addEventListener('mouseover', () => {
-            // const havePion = (td.innerHTML.includes("img"));
             const havePion = (td.innerHTML.includes("pion"));
             if (monTour && !havePion) {
+                // Possibilité de jouer
                 td.style.animation = animation;
                 td.style.cursor = "cell";
             }
             else if (!monTour && !havePion) {
+                // Attente de son tour
                 td.style.animation = "";
                 td.style.cursor = "wait";
             }
             else {
+                // Case remplie
                 td.style.animation = "";
                 td.style.cursor = "no-drop";
             }
         });
         td.addEventListener('mouseleave', () => {
+            // Souris plus là
             td.style.animation = "";
         });
     }
@@ -238,73 +237,83 @@ async function Jouer(colonne) {
     if (!monTour) {
         return;
     }
-
-    console.log("JOUER " + colonne);
     const Id = localStorage.getItem("id");
 
     apicall = await fetch(urlAPI + "jouer&position=" + colonne + "&identifiant=" + Id);
     reponse = await apicall.json();
-    console.log(reponse);
-    monTour = false;
+
+    console.log("Jouer(" + colonne + ") : ", reponse);
+    
+    monTour = (joueurIndex == reponse.tour);
+    
     updatePlateau(reponse.carte);
 
     if (reponse.etat.includes("gagne")) {
-        GameEnd(reponse.etat.includes("1") && reponse.joueur == "1" || 
-                reponse.etat.includes("2") && reponse.joueur == "2"); 
+        GameEnd(reponse.etat.includes(localStorage.getItem("joueur")));
     }
-
 }
 
-async function GameEnd(IamWinner)
+function GameEnd(IamWinner)
 {
     gameEnd = true;
-    // Get the modal
-    var modal = document.getElementById("myModal");
-    var stateText = document.getElementById("state");
-
-    console.log("Game End : ", IamWinner);
     
     if (IamWinner != null) {
-        if (IamWinner)
-        {
-            stateText.textContent = "Vous avez gagné !";
-            // alert("Tu as gagné ! ");
+        if (IamWinner) {
+            showModal("Vous avez gagné !", 2000);
+
         }
-        else
-        {
-            stateText.textContent = "Vous avez perdu...";
-            // alert("dommage t'as perdu...");
+        else {
+            showModal("Vous avez perdu...", 2000);
         }
     }
     else {
-        stateText.textContent = "il a ragequit...";
-        // alert();
+        showModal("il a ragequit...", 2000);
     }
-    await sleep(2000);
+}
+
+
+async function showModal(message, wait) {
+    var modal = document.getElementById("myModal");
+    var stateText = document.getElementById("state");
+
+    stateText.textContent = message;
+
+    if(wait != undefined) {
+        await sleep(wait);
+    }
+
     modal.style.display = "block";
 }
 
+// Recherche de game après la participation
 async function WaitingGame() {
-    const Id = localStorage.getItem("id");
+    const id = localStorage.getItem("id");
     let apicall; 
     let reponse;
-    let etatSearch = document.getElementById("etat");
+    const etatSearch = document.getElementById("etat");
 
     do {
-        apicall = await fetch(urlAPI + "statut&identifiant=" + Id);
+        apicall = await fetch(urlAPI + "statut&identifiant=" + id);
         reponse = await apicall.json();
-        console.log(reponse);
+        console.log("Statut (Search Game) : ", reponse);
 
-        if (reponse.etat == "Attente supprimee") {
-            etatSearch.textContent = "Relance en cours...";
+        if (reponse.etat == etat.ATTENTESUPPRIMEE) {
+            etatSearch.textContent = "Relance en cours";
             etatSearch.style.animation = "shake-horizontal 0.8s cubic-bezier(0.455, 0.030, 0.515, 0.955) both";
-            await sleep(3000);
+
+            for (let index = 0; index < 3; index++) {
+                etatSearch.textContent += ".";
+                await sleep(1000);
+            }
+
+            // Relance la recherche comme elle a été supprimée
             Play();
             return;
         }
-
+        // Délai entre chaque tentative de lecture du statut
         await sleep(3000);
-    } while (reponse.etat == "En attente" || reponse.etat == "Attente supprimee")
+
+    } while (reponse.etat == etat.ENATTENTE)
 
     localStorage.setItem("joueur", parseInt(reponse.joueur));
 
@@ -319,11 +328,11 @@ async function Giveup() {
     console.log(reponse);
     
     if (reponse.etat.includes("gagne")) {
-        alert("Vous avez abandonné votre partie !");
+        ("Vous avez abandonné votre partie !");
         window.location.replace("menu.html");
     }
-    else if (reponse.etat == "KO") {
-        alert("Vous tout seul dans la partie, relancez-en une");
+    else if (reponse.etat == etat.KO) {
+        console.log("personne dans la partie");
         window.location.replace("menu.html");
     }
     else if (reponse.etat == "Abandon") {
@@ -377,16 +386,13 @@ function CreateAnimatedBackground() {
         portepion.style.left = xCoord + "vw";
         portepion.style.animation = "pion-vertical 4s linear infinite";
         portepion.style.animationDuration = 4 + Math.floor((Math.random() * 10)) + 's';
-        portepion.addEventListener("animationiteration", () => { replacePion(portepion); });
+        portepion.addEventListener("animationiteration", () => { 
+            xCoord = Math.floor((Math.random() * nbPions)) * (100/nbPions);
+            portepion.style.left = xCoord + "vw";
+        });
 
         document.getElementById("background").appendChild(portepion);
     }
-}
-
-
-function replacePion(pion) {
-    xCoord = Math.floor((Math.random() * nbPions)) * (100/nbPions);
-    pion.style.left = xCoord + "vw";
 }
 
 function SetupModal() {
@@ -403,15 +409,18 @@ function SetupModal() {
     }
 }
 
+// Script lancé au lancement de chaque page
 document.addEventListener("DOMContentLoaded", async function() {
-    // Script ajouté au lancement
     const urlActuelle = window.location.href;
     const pseudo = localStorage.getItem("pseudo");
+
     console.log("pseudo enregistré : " + pseudo);
     console.log(urlActuelle);
+
     // En fonction de la page
     if (urlActuelle.includes("index") || urlActuelle == "http://127.0.0.1:5500/") {
         CreateAnimatedBackground();
+
         if (localStorage.getItem("pseudo") != "") {
             console.log("Ancien pseudo : " + pseudo);
             document.getElementById("reconnexion").textContent = phraseReconnexion + pseudo;
@@ -427,38 +436,13 @@ document.addEventListener("DOMContentLoaded", async function() {
     }
     else if (urlActuelle.includes("jeu")) {
         SetupModal();
-        // Affiche qui est connecté
-        document.getElementById("connected").textContent = phraseConnecte + pseudo;
+        showWhoIsConnected(); // Affiche qui est connecté
         addInteractions();
-        while (true) {
+        // Main loop
+        while (!gameEnd) {
             refresh();
             await sleep(1000);
         } 
         
     }
 });
-
-
-
-                    
-
-                    
-
-
-
-
-// if (cellValue == 1) {
-//     pion.style.backgroundColor = pionInJ1;
-//     pion.style.borderColor = "#c20000";
-//     portepion.style.backgroundColor = pionOutJ1;
-//     // imgPion.src = "images/pion rouge.png";
-// } else if (cellValue == 2) {                
-//     pion.style.backgroundColor = pionInJ2;
-//     pion.style.borderColor = "#bfb400";
-//     portepion.style.backgroundColor = pionOutJ2;
-//     // imgPion.src = "images/pion orange.png";
-// }
-    // imgPion.style.width = '100%';
-    // imgPion.style.height = 'auto';
-    // imgPion.style.display = 'block';
-    // cell.appendChild(imgPion);
